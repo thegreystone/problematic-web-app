eb17d196202329849f6249095805c6846a62cbc5
Marcus Hirt <marcus@hirt.se>
2018-08-30 19:25:10 +0200

    Adding form for adding customers

---
+++ src/main/java/se/hirt/examples/problematicwebapp/servlet/AddServlet.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2018 Marcus Hirt
+ *                    www.hirt.se
+ *
+ * This software is free:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESSED OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright (C) Marcus Hirt, 2018
+ */
+package se.hirt.examples.problematicwebapp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import se.hirt.examples.problematicwebapp.data.Customer;
+import se.hirt.examples.problematicwebapp.data.DataAccess;
+import se.hirt.examples.problematicwebapp.data.ValidationException;
+import se.hirt.examples.problematicwebapp.rest.CustomerKeys;
+
+/**
+ * Servlet "Hello World". Will be available under /helloservlet.
+ * 
+ * @author Marcus Hirt
+ */
+@WebServlet(name = "HelloWorldServlet", urlPatterns = { "/addcustomer" })
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String fullName = req.getParameter(CustomerKeys.FULL_NAME);
+		String phoneNumber = req.getParameter(CustomerKeys.PHONE_NUMBER);
+		ServletOutputStream out = resp.getOutputStream();
+		String result = "";
+		try {
+			Customer.validate(fullName, phoneNumber);
+			Customer customer = DataAccess.createCustomer(fullName, phoneNumber);
+			result = "Created: " + customer;
+		} catch (ValidationException e) {
+			result = e.getMessage();
+		}
+		out.write(result.getBytes());
+		out.flush();
+		out.close();
+	}
+}
